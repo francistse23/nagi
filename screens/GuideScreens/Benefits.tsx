5fd690acfb87cdd905ed26ee2a4410984e09df0d
francistse-realm <wtse.ucsd@gmail.com>
2020-04-10 12:56:38 -0700

    Added library to handle swipe gesture.
    Added react-native-swipe-gestures to handle swipe in guide.

--- screens/GuideScreens/Benefits.tsx
+++ screens/GuideScreens/Benefits.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 import { FlatList } from "react-native";
-import {
-  FlingGestureHandler,
-  Directions,
-  State,
-} from "react-native-gesture-handler";
 
 import Constants from "../../constants";
 import {
@@ -13,10 +8,13 @@ import {
   SmallText,
   VerticalView,
 } from "../../styled-components";
+import GestureRecognizer, {
+  swipeDirections,
+} from "react-native-swipe-gestures";
 
 const { spacing } = Constants;
 
-export default function Benefits() {
+export default function Benefits({ navigation }) {
   const benefits = [
     "Understand your pain",
     "Lower your stress",
@@ -25,18 +23,47 @@ export default function Benefits() {
     "Reduce brain chatter",
   ];
 
+  const config = {
+    velocityThreshold: 0.3,
+    directionalOffsetThreshold: 80,
+  };
+
+  const handleSwipe = (gestureName: string) => {
+    const { SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+
+    switch (gestureName) {
+      case SWIPE_DOWN:
+      case SWIPE_RIGHT:
+        navigation.goBack();
+        // console.log("dismissing");
+        break;
+
+      // case SWIPE_LEFT:
+      //   this.setState({backgroundColor: 'blue'});
+      //   break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <VerticalView style={{ padding: spacing * 4 }}>
-      <MediumText style={{ marginBottom: spacing * 2 }}>
-        Some of the benefits of meditating...
-      </MediumText>
-      <FlatList
-        data={benefits}
-        keyExtractor={(item, index) => String(index)}
-        renderItem={({ item }) => (
-          <SmallText style={{ marginVertical: spacing }}>- {item}</SmallText>
-        )}
-      />
-    </VerticalView>
+    <GestureRecognizer
+      config={config}
+      onSwipe={(direction) => handleSwipe(direction)}
+      style={{ flex: 1 }}
+    >
+      <VerticalView style={{ padding: spacing * 4 }}>
+        <MediumText style={{ marginBottom: spacing * 2 }}>
+          Some of the benefits of meditating...
+        </MediumText>
+        <FlatList
+          data={benefits}
+          keyExtractor={(item, index) => String(index)}
+          renderItem={({ item }) => (
+            <SmallText style={{ marginVertical: spacing }}>- {item}</SmallText>
+          )}
+        />
+      </VerticalView>
+    </GestureRecognizer>
   );
 }
