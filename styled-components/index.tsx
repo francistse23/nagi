434c9be91166f799565a176cd404ef14a82a26a6
francistse-realm <wtse.ucsd@gmail.com>
2020-04-23 12:19:42 -0700

    Added yarn-error.log to .gitignore.

--- styled-components/index.tsx
+++ styled-components/index.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components/native";
 import Constants from "../constants";
 import { scale, verticalScale } from "../utilities/scale";
 
