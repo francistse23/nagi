1486d2cb8e25545c8b257236ac41fe1669a66d92
francistse-realm <wtse.ucsd@gmail.com>
2020-04-27 15:51:27 -0700

    Added test suite for calculate time remaining.

--- utilities/calculateTimeRemaining.ts
+++ utilities/calculateTimeRemaining.ts
@@ -2,7 +2,9 @@ export default function calculateTimeRemaining(time: number) {
   const mins = Math.floor(time / 60);
   const seconds = time % 60;
 
-  return `${time <= 0 ? "00:00" : mins >= 10 ? mins : `0${mins}`}:${
+  if (time <= 0) return "00:00";
+
+  return `${mins >= 10 ? mins : `0${mins}`}:${
     seconds.toString().length > 1 ? seconds : `0${seconds}`
   }`;
 }
