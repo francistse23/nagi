442b669448a5d62221e70a444d96200dda3229ec
francistse-realm <wtse.ucsd@gmail.com>
2020-04-28 14:05:19 -0700

    Fixing some styling inconsistencies with Home and Drawer.

--- __tests__/screens/MeditationScreen.test.js
+++ __tests__/screens/MeditationScreen.test.js
@@ -1,18 +1,10 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import MockedNavigator from "../navigation/MockedNavigator";
 import MeditationScreen from "../../screens/MeditationScreens/MeditationScreen";
 
 describe("Meditation screen tests", () => {
   it("matches snapshot", () => {
     const screen = renderer.create(MeditationScreen);
     expect(screen).toMatchSnapshot();
-  });
-
-  it("matches expected navigation behavior", () => {
-    const screen = renderer(
-      <MockedNavigator component={MeditationScreen} params={{ time: 300 }} />
-    ).toJSON();
-    expect(screen).toMatchSnapshot;
   });
 });
